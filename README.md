5a8a76c77292a3c6a47ce349bd9231626ed6e33d
Gunther0662 <nicholasdiaz17@yahoo.com>
2024-09-05 21:38:17 -0500

    added boiler code

---
+++ README.md
@@ -0,0 +1,21 @@
+## Post Up
+
+## Description
+
+## Table of Contents
+
+## Installation
+
+## Usage
+
+## Credits
+
+## License
+
+## Badges
+
+## Features
+
+## How to Contributes
+
+## Tests
---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequileze;
+
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+} else {
+    sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'postgres'
+    }
+    );
+}
+
+module.export = sequelize;
