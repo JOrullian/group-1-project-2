b1c85505c74c7cc85f949565dfa6db54a381bd39
Drew Easter <drewheaster@gmail.com>
2024-09-10 14:44:30 -0500

    updated some styling for dashboard. Added dynamic addition of login/signup popup. Added login-popup.js

--- public/js/dashboard-map.js
+++ public/js/dashboard-map.js
@@ -1,58 +1,87 @@
-// function getLocation(event) {
-//     event.preventDefault();
+// const init = require("connect-session-sequelize");
 
-//     if (navigator.geolocation) {
-//       // Get the current position of the user
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//             const latitude = position.coords.latitude;
-//             const longitude = position.coords.longitude;
+function getLocation() {
 
-//             // Display the coordinates
-//             console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-//             console.log("Location found!");
-//         },
-//         (error) => {
-//           // Handle any errors that occur
-//           switch (error.code) {
-//             case error.PERMISSION_DENIED:
-//               console.log("User denied the request for Geolocation.");
-//               break;
-//             case error.POSITION_UNAVAILABLE:
-//               console.log("Location information is unavailable.");
-//               break;
-//             case error.TIMEOUT:
-//               console.log("The request to get user location timed out.");
-//               break;
-//             case error.UNKNOWN_ERROR:
-//               console.log("An unknown error occurred.");
-//               break;
-//           }
-//         }
-//       );
-//     } else {
-//       // If the browser does not support geolocation
-//       console.log("Geolocation is not supported by this browser.");
-//     }
-// };
+    if (navigator.geolocation) {
+      // Get the current position of the user
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
 
-function initMap() {
-    // The location for the center of the map (latitude and longitude)
-    let location = { lat: 33.0050, lng: -96.5698 };
-    
-    // The map, centered at the location
-    const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: location,
-        disableDefaultUI: true,
-    });
-    
-    // The marker, positioned at the location
-    const marker = new google.maps.Marker({
-        position: location,
-        map: map,
-    });
+            // Display the coordinates
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            console.log("Location found!");
+
+            function initMap() {
+                // The location for the center of the map (latitude and longitude)
+                let location = { lat: latitude, lng: longitude };
+                
+                // The map, centered at the location
+                const map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 12,
+                    center: location,
+                    disableDefaultUI: true,
+                });
+                
+                // The circle, positioned at the location
+                const cityCircle = new google.maps.Circle({
+                    strokeColor: "#4285f4",
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: "#4285f4",
+                    fillOpacity: 0.35,
+                    map,
+                    center: location,
+                    radius: 10000,
+                  });
+            };
+
+            initMap();
+        },
+        (error) => {
+          // Handle any errors that occur
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              console.log("User denied the request for Geolocation.");
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.log("Location information is unavailable.");
+              break;
+            case error.TIMEOUT:
+              console.log("The request to get user location timed out.");
+              break;
+            case error.UNKNOWN_ERROR:
+              console.log("An unknown error occurred.");
+              break;
+          }
+        }
+      );
+    } else {
+      // If the browser does not support geolocation
+      console.log("Geolocation is not supported by this browser.");
+    }
 };
 
-// Load the map once the page has fully loaded
-window.onload = initMap;
+// getLocation();
+
+// function initMap() {
+//     // The location for the center of the map (latitude and longitude)
+//     let location = { lat: 33.0050, lng: -96.5698 };
+    
+//     // The map, centered at the location
+//     const map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 16,
+//         center: location,
+//         disableDefaultUI: true,
+//     });
+    
+//     // The marker, positioned at the location
+//     const marker = new google.maps.Marker({
+//         position: location,
+//         map: map,
+//     });
+// };
+
+// // Load the map once the page has fully loaded
+// window.onload = initMap;
---
+++ public/js/login-popup.js
@@ -0,0 +1,65 @@
+const moreInfoBtn = document.getElementById('more-info-btn');
+const mainContainer = $('#main-container');
+
+let userLoggedIn = true
+
+moreInfoBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if(userLoggedIn === true) {
+        console.log('User logged in!')
+    } else if(userLoggedIn === false) {
+        mainContainer.append(
+        `<div id="login-popup-container" class="login-popup-container">
+            <div class="login-popup-window">
+                <div class="popup-login-side">
+                    <img id="login-close-btn" class="login-close-btn" src="../../public/icons/close-btn-icon.svg">
+                    <div class="login-window">
+                        <h1 class="login-form-title">Welcome to<br/><span class="login-brand-name">QuickMatch</span></h1>
+                        <form id="login-form" class="login-form">
+                            <input class="login-inputs" type="text" placeholder="Username" required>
+                            <input class="login-inputs" type="password" placeholder="Password" required>
+                            <button id="login-button" class="login-button">Login</button>
+                        </form>                            
+                    </div>
+                </div>
+                <div class="popup-signup-side">
+                    <div class="signup-window">
+                        <h1 class="signup-form-title">Need an account?</h1>
+                        <button id="signup-button" class="signup-button">Sign Up!</button>                    
+                    </div>
+                </div>
+            </div>
+        </div>`
+        );
+        const loginPopUpCont = $('#login-popup-container');
+        const loginExitBtn = $('#login-close-btn')
+        
+        loginExitBtn.on('click', () => {
+            loginPopUpCont.remove();
+        })
+
+    }
+})
+
+/* <div class="login-popup-container">
+    <div class="login-popup-window">
+        <div class="popup-login-side">
+            <img class="login-close-btn" src="../../public/icons/close-btn-icon.svg">
+            <div class="login-window">
+                <h1 class="login-form-title">Welcome to<br/><span class="login-brand-name">QuickMatch</span></h1>
+                <form id="login-form" class="login-form">
+                    <input class="login-inputs" type="text" placeholder="Username" required>
+                    <input class="login-inputs" type="password" placeholder="Password" required>
+                    <button id="login-button" class="login-button">Login</button>
+                </form>                            
+            </div>
+        </div>
+        <div class="popup-signup-side">
+            <div class="signup-window">
+                <h1 class="signup-form-title">Need an account?</h1>
+                <button id="signup-button" class="signup-button">Sign Up!</button>                    
+            </div>
+        </div>
+    </div>
+</div> */
