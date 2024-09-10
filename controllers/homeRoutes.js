26823f7b98420e8d236b12f7e3f32afa59078bbd
[Jeremy Ray] <jeremydray@gmail.com>
2024-09-09 19:46:10 -0500

    Added initial homeroutes to controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { User, Event, Group } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const eventList = await Event.findAll();
+
+    const events = eventList.map((event) => event.get({ plain: true }));
+    res.status(200)
+    res.json(events)
+
+    // res.render('dashboard', { 
+    //   events
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/event/:id', async (req, res) => {
+  try {
+    const eventData = await Event.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const events = await eventData.get({ plain: true });
+
+    res.json(events, )
+    //     {
+    //   logged_in: req.session.logged_in
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+module.exports = router;
