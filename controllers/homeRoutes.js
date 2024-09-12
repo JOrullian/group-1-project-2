65c2e2891a4c567a73360ad427ea44b865f67dd8
JOrullian <jedediahorullian@gmail.com>
2024-09-12 08:42:42 -0500

    added login route to render page, now the raw dashboard page will show, but it is pulling from the current blank dashboard, so it looks like garbage.  At least it shows up now when you start the app and check it out at localhost.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,17 +10,17 @@ router.get('/api-key', (req, res) => {
 });
 
 // Pull list of events for dashboard
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const eventList = await Event.findAll();
 
     const events = eventList.map((event) => event.get({ plain: true }));
 
-    res.status(200).json(events);
+    // res.status(200).json(events);
 
     console.log(events);
 
-    // res.render('dashboard', { events, logged_in: req.session.logged_in });
+    res.render('dashboard', { events, logged_in: req.session.logged_in });
 
   } catch (err) {
     res.status(500).json(err);
@@ -28,7 +28,7 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 // Pull specific event that user selects
-router.get('/event/:id', withAuth, async (req, res) => {
+router.get('/event/:id', async (req, res) => {
   try {
     const eventData = await Event.findByPk(req.params.id);
 
@@ -37,11 +37,21 @@ router.get('/event/:id', withAuth, async (req, res) => {
     }
     const event = await eventData.get({ plain: true });
 
-    // res.render('event', { event, logged_in: req.session.logged_in });
+    res.render('event', { ...event, logged_in: req.session.logged_in });
 
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+})
 
 module.exports = router;
