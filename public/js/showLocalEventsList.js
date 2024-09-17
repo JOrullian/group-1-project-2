eb95c6cdbc2f6f1749f95eaf2725eb4ac46b90ba
JOrullian <jedediahorullian@gmail.com>
2024-09-17 07:37:45 -0500

    fixed where seed data was getting erased everytime you do npm start.  Was an issue with setting either the sequelize sync to false in seed.js or sequelize sync to true in server.js.

--- public/js/showLocalEventsList.js
+++ public/js/showLocalEventsList.js
@@ -1,49 +1,48 @@
-const mapCont = $('#map-container');
+const mapCont = $("#map-container");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const eventsData = localStorage.getItem("nearbyEvents");
-  if (eventsData) {
-    const events = JSON.parse(eventsData);
-    console.log("Retrieved events:", events);
+const eventsData = localStorage.getItem("nearbyEvents");
+if (eventsData) {
+  const events = JSON.parse(eventsData);
+  console.log("Retrieved events:", events);
 
-    // Define sport type to icon mappings
-    const sportIcons = {
-      basketball: "basketball-icon.svg",
-      soccer: "soccer-icon.svg",
-      football: "football-icon.svg",
-      pickleball: "pickleball-icon.svg",
-      tennis: "tennis-icon.svg",
-      raquetball: "raquetball-icon.svg",
-      baseball: "baseball-icon.svg",
-      volleyball: "volleyball-icon.svg",
-      lacrosse: "lacrosse-icon.svg",
-      hockey: "hockey-icon.svg"
-    };
+  // Define sport type to icon mappings
+  const sportIcons = {
+    basketball: "basketball-icon.svg",
+    soccer: "soccer-icon.svg",
+    football: "football-icon.svg",
+    pickleball: "pickleball-icon.svg",
+    tennis: "tennis-icon.svg",
+    raquetball: "raquetball-icon.svg",
+    baseball: "baseball-icon.svg",
+    volleyball: "volleyball-icon.svg",
+    lacrosse: "lacrosse-icon.svg",
+    hockey: "hockey-icon.svg",
+  };
 
-    // Populate events in the .local-events-list container
-    const eventsContainer = document.querySelector(".local-events-list");
-    eventsContainer.innerHTML = ""; // Clear existing events
+  // Populate events in the .local-events-list container
+  const eventsContainer = document.querySelector(".local-events-list");
+  eventsContainer.innerHTML = ""; // Clear existing events
 
-    events.forEach((event) => {
-      const eventElement = document.createElement("div");
-      eventElement.classList.add("event-container");
+  events.forEach((event) => {
+    const eventElement = document.createElement("div");
+    eventElement.classList.add("event-container");
 
-      const dateTimeString = event.time;
-      const dateObject = new Date(dateTimeString);
+    const dateTimeString = event.time;
+    const dateObject = new Date(dateTimeString);
 
-      const openSlots = event.numberOfPlayers - event.participants.length;
+    const openSlots = event.numberOfPlayers - event.participants.length;
 
-      // Format Date
-      const formattedDate = dateObject.toLocaleDateString();
-      const formattedTime = dateObject.toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
+    // Format Date
+    const formattedDate = dateObject.toLocaleDateString();
+    const formattedTime = dateObject.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
 
-      // Determine the icon based on sportType
-      const iconFileName = sportIcons[event.sportType] || "default-icon.svg"; // Default to 'default-icon.svg' if sportType is not found
+    // Determine the icon based on sportType
+    const iconFileName = sportIcons[event.sportType] || "default-icon.svg"; // Default to 'default-icon.svg' if sportType is not found
 
-      eventElement.innerHTML = `
+    eventElement.innerHTML = `
         <div class="event-icon-container">
           <img src="icons/${iconFileName}" class="event-icon">
         </div>
@@ -73,17 +72,19 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>
       `;
 
-      // Append each event to the container
-      eventsContainer.appendChild(eventElement);
+    // Append each event to the container
+    eventsContainer.appendChild(eventElement);
 
-      if ($(window).width() < 600) {
-        eventElement.addEventListener("click", () => {
-          // Check if the details div already exists, if so, remove it
-          const eventDetails = eventElement.querySelector(".event-details-container");
-          if (eventDetails) {
-            eventDetails.remove(); // Remove existing details if present
-          } else {
-            mapCont.append(`
+    if ($(window).width() < 600) {
+      eventElement.addEventListener("click", () => {
+        // Check if the details div already exists, if so, remove it
+        const eventDetails = eventElement.querySelector(
+          ".event-details-container"
+        );
+        if (eventDetails) {
+          eventDetails.remove(); // Remove existing details if present
+        } else {
+          mapCont.append(`
               <div id="event-details-container" class="event-details-container">
                 <img id="event-close-btn" class="event-close-btn" src="/icons/close-btn-icon.svg">
                 <div class="event-title-container">
@@ -101,57 +102,60 @@ document.addEventListener("DOMContentLoaded", () => {
               </div>
             `);
 
-            // Close button functionality
-            $(".event-close-btn").on("click", () => {
-              $(".event-details-container").remove(); // Close the popup
-            });
+          // Close button functionality
+          $(".event-close-btn").on("click", () => {
+            $(".event-details-container").remove(); // Close the popup
+          });
 
-            // Handle the click on the "Join Event" button
-            $(".join-event-btn").on("click", async function () {
-              const eventId = $(this).data("event-id");
+          // Handle the click on the "Join Event" button
+          $(".join-event-btn").on("click", async function () {
+            const eventId = $(this).data("event-id");
 
-              try {
-                const response = await fetch(`/api/events/${eventId}/join`, {
-                  method: "PUT",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                });
+            try {
+              const response = await fetch(`/api/events/${eventId}/join`, {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
 
-                if (response.ok) {
-                  alert("You've successfully joined the event!");
+              if (response.ok) {
+                alert("You've successfully joined the event!");
 
-                  // Update the UI to reflect the new participant count
-                  event.participants.push("newUser"); // Replace 'newUser' with the actual user data
-                  const newOpenSlots = event.numberOfPlayers - event.participants.length;
-                  eventElement.querySelector(".event-slots").textContent = newOpenSlots;
+                // Update the UI to reflect the new participant count
+                event.participants.push("newUser"); // Replace 'newUser' with the actual user data
+                const newOpenSlots =
+                  event.numberOfPlayers - event.participants.length;
+                eventElement.querySelector(".event-slots").textContent =
+                  newOpenSlots;
 
-                  console.log(event.participants);
-
-                } else if (response.status === 400) {
-                  alert("You are already part of this event.");
-                } else if (response.status === 403) {
-                  alert("This event is already full.");
-                } else {
-                  alert("Failed to join the event.");
-                }
-              } catch (error) {
-                console.error("Error joining the event:", error);
-                alert("An error occurred while joining the event.");
+                console.log(event.participants);
+              } else if (response.status === 400) {
+                alert("You are already part of this event.");
+              } else if (response.status === 403) {
+                alert("This event is already full.");
+              } else {
+                alert("Failed to join the event.");
               }
-            });
-          }
-        });
-      } else {
-        const moreInfoBtn = eventElement.querySelector(".more-info-btn");
+            } catch (error) {
+              console.error("Error joining the event:", error);
+              alert("An error occurred while joining the event.");
+            }
+          });
+        }
+      });
+    } else {
+      const moreInfoBtn = eventElement.querySelector(".more-info-btn");
 
-        moreInfoBtn.addEventListener("click", async () => {
-          // Check if the details div already exists, if so, remove it
-          const eventDetails = eventElement.querySelector(".event-details-container");
-          if (eventDetails) {
-            eventDetails.remove(); // Remove existing details if present
-          } else {
-            mapCont.append(`
+      moreInfoBtn.addEventListener("click", async () => {
+        // Check if the details div already exists, if so, remove it
+        const eventDetails = eventElement.querySelector(
+          ".event-details-container"
+        );
+        if (eventDetails) {
+          eventDetails.remove(); // Remove existing details if present
+        } else {
+          mapCont.append(`
               <div id="event-details-container" class="event-details-container">
                 <img id="event-close-btn" class="event-close-btn" src="/icons/close-btn-icon.svg">
                 <div class="event-title-container">
@@ -169,50 +173,50 @@ document.addEventListener("DOMContentLoaded", () => {
               </div>
             `);
 
-            // Close button functionality
-            $("#event-close-btn").on("click", () => {
-              $("#event-details-container").remove(); // Close the popup
-            });
+          // Close button functionality
+          $("#event-close-btn").on("click", () => {
+            $("#event-details-container").remove(); // Close the popup
+          });
 
-            // Handle the click on the "Join Event" button
-            $(".join-event-btn").on("click", async function () {
-              const eventId = $(this).data("event-id");
+          // Handle the click on the "Join Event" button
+          $(".join-event-btn").on("click", async function () {
+            const eventId = $(this).data("event-id");
 
-              try {
-                const response = await fetch(`/api/events/${eventId}/join`, {
-                  method: "PUT",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                });
+            try {
+              const response = await fetch(`/api/events/${eventId}/join`, {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
 
-                if (response.ok) {
-                  alert("You've successfully joined the event!");
+              if (response.ok) {
+                alert("You've successfully joined the event!");
 
-                  // Update the UI to reflect the new participant count
-                  event.participants.push("newUser"); // Replace 'newUser' with the actual user data
-                  const newOpenSlots = event.numberOfPlayers - event.participants.length;
-                  eventElement.querySelector(".event-slots").textContent = newOpenSlots;
+                // Update the UI to reflect the new participant count
+                event.participants.push("newUser"); // Replace 'newUser' with the actual user data
+                const newOpenSlots =
+                  event.numberOfPlayers - event.participants.length;
+                eventElement.querySelector(".event-slots").textContent =
+                  newOpenSlots;
 
-                  console.log(event.participants);
-
-                } else if (response.status === 400) {
-                  alert("You are already part of this event.");
-                } else if (response.status === 403) {
-                  alert("This event is already full.");
-                } else {
-                  alert("Failed to join the event.");
-                }
-              } catch (error) {
-                console.error("Error joining the event:", error);
-                alert("An error occurred while joining the event.");
+                console.log(event.participants);
+              } else if (response.status === 400) {
+                alert("You are already part of this event.");
+              } else if (response.status === 403) {
+                alert("This event is already full.");
+              } else {
+                alert("Failed to join the event.");
               }
-            });
-          }
-        });
-      }
-    });
-  } else {
-    console.log("No nearby events data found in local storage.");
-  }
-});
+            } catch (error) {
+              console.error("Error joining the event:", error);
+              alert("An error occurred while joining the event.");
+            }
+          });
+        }
+      });
+    }
+  });
+} else {
+  console.log("No nearby events data found in local storage.");
+}
