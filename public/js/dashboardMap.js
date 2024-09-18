26016a1f32a3846437cfa08b53a88fe15f5b4cc0
[Jeremy Ray] <jeremydray@gmail.com>
2024-09-17 19:57:11 -0500

    Fixed formatting on all map popup iterations

--- public/js/dashboardMap.js
+++ public/js/dashboardMap.js
@@ -221,14 +221,19 @@ function showLocalEventsList(events) {
               <div id="event-details-container" class="event-details-container">
                 <img id="event-close-btn" class="event-close-btn" src="/icons/close-btn-icon.svg">
                 <div class="event-title-container">
-                  <h1 class="event-title">${event.name}<br/>${event.location}</h1>
+                  <h1 class="event-title">${event.name}</h1>
+                </div>
+                <div class="event-subtitletitle-container">
+                  <h2 class="event-subtitle">${event.location}</h2>
                 </div>
                 <div class="event-details-divider"></div>
                 <div class="event-details-info">
                   <div class="event-start-time-container">
                     <h3 class="event-start-time">Start Time: ${formattedTime}, ${formattedDate}</h3>
                   </div>
-                  <div class="event-join-container">
+                </div>
+                <div class="event-details-divider"></div>
+                <div class="event-join-container">
                     <button class="join-event-btn" data-event-id="${event.id}">Join Event</button>
                   </div>
                 </div>
@@ -445,16 +450,23 @@ function showYourEventsList(yourEvents) {
               <div id="event-details-container" class="event-details-container">
                 <img id="event-close-btn" class="event-close-btn" src="/icons/close-btn-icon.svg">
                 <div class="event-title-container">
-                  <h1 class="event-title">${event.name}<br/>${event.location}</h1>
+                  <h1 class="event-title">${event.name}</h1>
+                </div>
+                <div class="event-subtitletitle-container">
+                  <h2 class="event-subtitle">${event.location}</h2>
                 </div>
                 <div class="event-details-divider"></div>
                 <div class="event-details-info">
                   <div class="event-start-time-container">
                     <h3 class="event-start-time">Start Time: ${formattedTime}, ${formattedDate}</h3>
                   </div>
-                  <div class="event-join-container">
+                </div>
+                <div class="event-details-divider"></div>
+                <div class="event-join-container">
                     <button class="join-event-btn" data-event-id="${event.id}">Join Event</button>
                   </div>
+                </div>
+              </div>
                 </div>
               </div>
             `);
@@ -516,14 +528,19 @@ function showYourEventsList(yourEvents) {
               <div id="event-details-container" class="event-details-container">
                 <img id="event-close-btn" class="event-close-btn" src="/icons/close-btn-icon.svg">
                 <div class="event-title-container">
-                  <h1 class="event-title">${event.name} - ${event.location}</h1>
+                  <h1 class="event-title">${event.name}</h1>
+                </div>
+                <div class="event-subtitletitle-container">
+                  <h2 class="event-subtitle">${event.location}</h2>
                 </div>
                 <div class="event-details-divider"></div>
                 <div class="event-details-info">
                   <div class="event-start-time-container">
                     <h3 class="event-start-time">Start Time: ${formattedTime}, ${formattedDate}</h3>
                   </div>
-                  <div class="event-join-container">
+                </div>
+                <div class="event-details-divider"></div>
+                <div class="event-join-container">
                     <button class="join-event-btn" data-event-id="${event.id}">Join Event</button>
                   </div>
                 </div>
